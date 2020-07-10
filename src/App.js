0392202d0cd89e064242be47b0dcfac21eaa9e7a
arafeen <arafeenbd@yahoo.com>
2020-07-09 20:04:52 -0400

    call twilio api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
+import StartForm from './StartForm';
+import TwilioVideo from "twilio-video"
+
+const Video = ({ token }) => {
+  const localVidRef = useRef()
+  const remoteVidRef = useRef()
+
+  useEffect(() => {
+    TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
+      room => {
+        // Attach the local video
+        TwilioVideo.createLocalVideoTrack().then(track => {
+          localVidRef.current.appendChild(track.attach())
+        })
+
+        const addParticipant = participant => {
+          console.log("new participant!")
+          console.log(participant)
+          participant.tracks.forEach(publication => {
+            if (publication.isSubscribed) {
+              const track = publication.track
+
+              remoteVidRef.current.appendChild(track.attach())
+              console.log("attached to remote video")
+            }
+          })
+
+          participant.on("trackSubscribed", track => {
+            console.log("track subscribed")
+            remoteVidRef.current.appendChild(track.attach())
+          })
+        }
+
+        room.participants.forEach(addParticipant)
+        room.on("participantConnected", addParticipant)
+      }
+    )
+  }, [token])
+
+  return (
+    <div>
+      <div ref={localVidRef} />
+      <div ref={remoteVidRef} />
+    </div>
+  )
+}
 
 function App() {
+  const [token, setToken] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        {!token ? <StartForm storeToken={setToken} /> : <Video token={token} />}
+        </div>
       </header>
     </div>
   );
